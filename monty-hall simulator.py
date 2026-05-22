6b6d686bcd37b1dbc4960888d7a392daadcb56f0
Arushias <aseh0014@student.monash.edu>
2026-05-22 16:23:59 +1000

    Add files via upload

---
+++ monty-hall simulator.py
@@ -0,0 +1,24 @@
+import random
+
+iterations = 100000
+car_count = 0
+none_count = 0
+
+for i in range(iterations):
+    doors = ['none', 'none', 'none']
+    doors[random.randint(0,2)] = 'car'
+
+    if doors[1] == 'none':
+        doors.pop(1)
+    elif doors[2] == 'none':
+        doors.pop(2)
+
+    chosen_door = doors[1]
+
+    if chosen_door == 'car':
+        car_count += 1
+    else:
+        none_count += 1
+
+print(f'Car: {car_count} = {round(car_count/iterations*100, 1)}%')
+print(f'None: {none_count} = {round(none_count/iterations*100, 1)}%')
